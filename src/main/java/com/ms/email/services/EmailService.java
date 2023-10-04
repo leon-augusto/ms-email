0a127209fb2445c7c8d18f16a20b814ea26aaa14
leon <leonaugusto087@gmail.com>
2023-10-04 11:08:51 -0300

    chore: updated business rules

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -4,12 +4,17 @@ import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmailService {
@@ -20,6 +25,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Transactional
     public EmailModel sendEmail(EmailModel emailModel) {
         emailModel.setSendDateEmail(LocalDateTime.now());
         try{
@@ -36,5 +42,13 @@ public class EmailService {
         } finally {
             return emailRepository.save(emailModel);
         }
+    }
+
+    public Page<EmailModel> findAll(Pageable pageable){
+        return emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId){
+        return emailRepository.findById(emailId);
     }
 }
